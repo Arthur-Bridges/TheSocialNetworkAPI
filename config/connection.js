03db275243cb999a129e56176d6ed72719e0253b
Arthur Bridges <arthurbridges08@yahoo.com>
2024-01-08 20:23:31 -0600

    fixing import using es6'

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 import { connect, connection } from "mongoose";
 //TODO: set the connection => database
-connect("mongodb://127.0.0.1:27017/TheSocialNetworkAPI");
+const connectDB = await connect("mongodb://127.0.0.1:27017/TheSocialNetworkAPI");
 
-export default connection;
+export default connectDB;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 // import { MongoClient } from "mongodb";
 import db from "./config/connection.js";
-import routes from "./routes";
+import routes from "./routes/index.js";
 
 const app = express();
 const PORT = 3001;
